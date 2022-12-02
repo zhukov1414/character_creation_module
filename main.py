28b1b229863f696425924c8ec92a06837541980d
Евгений Жуков <zhukov1414@yandex.ru>
2022-12-02 19:14:30 +0300

    New dir

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def start_training(char_name: str, char_class: str) -> str:
           'defence — чтобы блокировать атаку противника или special — чтобы'
           'использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd = None
+    cmd: str = None
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -59,9 +59,9 @@ def start_training(char_name: str, char_class: str) -> str:
     return 'Тренировка окончена.'
 
 
-def choice_char_class() -> bool:
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    approve_choice: str = None
+    char_class: str = None
     while approve_choice != 'y':
         char_class = input('Введи название персонажа,')
         ('за которого хочешь играть:')
@@ -83,12 +83,12 @@ def choice_char_class() -> bool:
 def main():
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель - warrior, Маг - mage, Лекарь - healer')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
 
